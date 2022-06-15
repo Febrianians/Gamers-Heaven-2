4342ec77bb6203e16b13d45bc03f35a0e8145e20
bang-eric <ezraricardo@gmail.com>
2022-06-15 10:48:52 +0700

    update content & foldering landing-page, change style with scss

--- components/header.jsx
+++ components/header.jsx
@@ -1,9 +1,62 @@
+import { Navbar, NavbarBrand, NavbarToggler, Collapse, Nav, NavItem, NavLink } from 'reactstrap';
+
+
 export default function Header(props) {
     return(
-        <nav className="navbar navbar-dark bg-primary">
-            <div className="row col-12 d-flex justify-content-center text-white">
-            <span className="h3">{props.title}</span>
-            </div>
-        </nav>
+        <div>
+            <Navbar
+            color="dark"
+            dark
+            expand="md"
+            fixed="off"
+            className='navbar'
+            >
+                <NavbarBrand className='navbrand' href="/landing-page">
+                    {props.title}
+                </NavbarBrand>
+                <NavbarToggler onClick={function noRefCheck(){}} />
+                <Collapse navbar>
+                    <Nav
+                    className="me-auto"
+                    navbar
+                    >
+                    <NavItem className='navitem'>
+                        <NavLink href="/landing-page">
+                        HOME
+                        </NavLink>
+                    </NavItem>
+                    <NavItem className='navitem'>
+                        <NavLink href="/game-list-page">
+                        LIST GAME
+                        </NavLink>
+                    </NavItem>
+                    <NavItem className='navitem'>
+                        <NavLink href="/game-detail-page">
+                        LIST DETAIL GAME
+                        </NavLink>
+                    </NavItem>
+                    <NavItem className='navitem'>
+                        <NavLink href="https://github.com/reactstrap/reactstrap">
+                        ABOUT ME
+                        </NavLink>
+                    </NavItem>
+        
+                    </Nav>
+                    <Nav>
+                    <NavItem className='navitem'>
+                        <NavLink className='navlink' href='/register-page'>
+                        REGISTER
+                        </NavLink>
+                    </NavItem>
+                    <NavItem className='navitem'>
+                        <NavLink className='navlink' href="/login-page">
+                        LOGIN
+                        </NavLink>
+                    </NavItem>
+        
+                    </Nav>
+                </Collapse>
+            </Navbar>
+        </div>
     )
 }
--- pages/landing-page.jsx
+++ pages/landing-page.jsx
@@ -1,36 +1,10 @@
-// import NavbarComponent from "../components/navbarLanding";
-import Header from '../components/header';
-import { Button } from 'reactstrap';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
-import styles from '../styles/landingPage.module.css';
+import LandingPageComponent from "../components/landing-page/landing-page";
 
 export default function LandingPage() {
 
   return(
     <>
-      <div className={styles.landingpage}>
-        <Header title='Landing Page'/>
-        <div className={styles.wrapperUp}>
-          <div className={styles.content}>
-            <h1 className={styles.contenth1}>PLAY TRADITIONAL GAME</h1>
-            <h3 className={styles.contenth3}>
-              Experience New Traditional Game Play
-            </h3>
-            <div>
-              <Button color="warning" size="lg">
-                PLAY NOW
-              </Button>
-            </div>
-          </div>
-        </div>
-        <div className={styles.wrapperDown}>
-          <h6>THE STORY</h6>
-          <a className={styles.facaretdown} href="/">
-            <FontAwesomeIcon icon={faCaretDown} />
-          </a>
-        </div>
-      </div>
+      <LandingPageComponent />
     </>
   )
 }
