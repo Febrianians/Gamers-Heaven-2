5e918df27d7888c9c1a35164d26940196c3a730b
iannhd <in.hadyan@gmail.com>
2022-06-15 13:59:12 +0700

    first push

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import '../components/profile-page/profilePageStyle.scss'
 import '../components/landing-page/landingPageStyle.scss'
 import '../components/headerStyle.scss'
 import '../components/home-page/homePageStyle.scss'
+import '../components/profile-page-update/profilePageUpdateStyle.scss'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
