ec8ef6d1d8dacf2daca48bddee6ab2dc3cff04e0
iannhd <in.hadyan@gmail.com>
2022-06-15 12:41:02 +0700

    adding profile page structure with placeholder

--- components/profile-page/content.jsx
+++
@@ -1,9 +0,0 @@
-
-
-export default function ContentComponent() {
-    return (
-        <>
-        <h1>Content</h1>
-        </>
-    )
-}
--- components/profile-page/profile-page.jsx
+++ components/profile-page/profile-page.jsx
@@ -1,18 +1,38 @@
-import ContentComponent from "./content";
-import SidebarComponent from "./sidebar";
-
+import Header from '../header'
 
 export default function ProfilePageComponent() {
     return (
         <>
-        <div className="container">
-                <div className="sidebar">
-                    <SidebarComponent/>
+        <div className="profilePage">
+            <Header title='Profile Page'/>
+            <div className="wrapper">
+                <div className="card">
+                    <div className="cardTitle">
+                        <div className="profilePicture">
+                            <div className="image"></div>
+                        </div>
+                       
+                    </div>
+                    <div className="cardContent">
+                        <div className="username">
+                             <div><h3>User #1</h3></div>
+                        </div>
+                        <div className="email">
+                            <h3>Email</h3>
+                            <h6>Test@mail.com</h6>
+                        </div>
+                        <div className="gamePlayed">
+                            <div className="game">
+                                <h3>Game Played</h3>
+                            </div>
+                            <div className="playedGame">
+                                <h6>- Rock Paper Scissors : High Score (30pts)</h6>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div className="content">
-                    <ContentComponent/>
                 </div>
-        </div>
+            </div>
         </>
     )
 }
