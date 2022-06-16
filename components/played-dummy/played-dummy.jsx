fe76728ea005fc970ee82557d2fced0cce3565ec
bang-eric <ezraricardo@gmail.com>
2022-06-16 13:53:29 +0700

    add routing to game dummy & dummy played count

---
+++ components/played-dummy/played-dummy.jsx
@@ -0,0 +1,8 @@
+export default function GameRPSComponent() {
+
+    return(
+        <>
+            <h1>Game RPS</h1>
+        </>
+    )
+}
--- pages/game-list.jsx
+++ pages/game-list.jsx
@@ -12,10 +12,21 @@ import {
 } from "reactstrap";
 import Link  from "next/link";
 import { useRouter } from "next/router";
+import Header from "../components/headerLogin";
 
 const GameList = () => {
   const [gameList, setGameList] = useState([]);
-  const router = useRouter
+  const [gameUrl, setGameUrl] = useState('')
+  const router = useRouter()
+
+  function tambahGame() {
+    router.push('/game-form')
+  }
+
+  function handleGameUrl() {
+    // router.push(`/${gameUrl ? gameUrl : ""}`)
+    // console.log(game.game_url, '==> handleGame');
+  }
 
   useEffect(() => {
     onValue(
@@ -25,15 +36,18 @@ const GameList = () => {
         snapshot.forEach((childSnapshot) => {
           const childKey = childSnapshot.key;
           const childData = childSnapshot.val();
-          //   console.log(childKey, childData);
+            // console.log(childKey, childData, '==> key & data game-list');
           newGames.push({
             key: childKey,
             name: childData.name,
             game_url: childData.game_url,
             description: childData.description,
+            play_count: childData.play_count
           });
         });
+        // setGameUrl(game_url)
         setGameList(newGames);
+        console.log(gameList, '==> game list')
       },
       {
         onlyOnce: true,
@@ -42,36 +56,41 @@ const GameList = () => {
   }, []);
 
   return (
-    <Container>
-      <Link
-        className="btn btn-primary btn-lg mb-2"
-        role="button"
-        href="/game-form"
-      >
-        Tambah
-      </Link>
-      <Row>
-        {gameList &&
-          gameList.map((game) => {
-            return (
-              <Col key={game.key} sm="6">
-                <Card body>
-                  <CardTitle tag="h5">{game.name}</CardTitle>
-                  <CardText>{game.description}</CardText>
-                  <Button
-                    // role="button"
-                    // className="btn btn-secondary"
-                    // href={router.game.game_url ? game.game_url : "/games"}
-                  >
-                      {router.game.game_url ? game.game_url : "/games"}
-                      More..
-                  </Button>
-                </Card>
-              </Col>
-            );
-          })}
-      </Row>
-    </Container>
+    <>
+      <Header />
+      <Container style={{paddingTop:'50px'}}>
+        <Button style={{marginBottom:'50px'}} onClick={tambahGame}>
+          Tambah
+        </Button>
+        <Row>
+          {gameList &&
+            gameList.map((game) => {
+              return (
+                <Col key={game.key} sm="6">
+                  {/* {console.log(game.game_url, '==> handleGame')} */}
+                  <Card body>
+                    <CardTitle tag="h5">{game.name}</CardTitle>
+                    <CardText>{game.description}</CardText>
+
+                    <CardText>{game.play_count}</CardText>
+                    <Button
+                      // role="button"
+                      // className="btn btn-secondary"
+                      // href={router.game.game_url ? game.game_url : "/games"}
+                      onClick={(e) => {
+                        e.preventDefault()
+                        router.push(`/${game.game_url ? game.game_url : ""}`)}}
+              
+                    >
+                        More..
+                    </Button>
+                  </Card>
+                </Col>
+              );
+            })}
+        </Row>
+      </Container>
+    </>
   );
 };
 
